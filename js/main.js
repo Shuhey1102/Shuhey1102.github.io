440a48d9fb4a8bb3415bee5a4eac4c4d482181e4
kaji1102 <kajiwara.shuhei.1102kaji@gmail.com>
2026-04-21 02:11:43 +0900

    Blog項目追加、ロケール切替不具合対応

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 'use strict';
+import {messages} from './message.js';
 
 // get user's locale and set lang attribute
 const lang = navigator.language.startsWith('ja') ? 'ja' : 'en';
 document.documentElement.lang = lang;
 
-// change text content based on locale
-fetch(`./locales/${lang}.json`)
-  .then(res => res.json())
-  .then(data => {
+
+window.onload = () => {
+    // change text content based on locale
     document.querySelectorAll('[data-text]').forEach(el => {
       const key = el.dataset.text;
-      if (data[key]) {
-        el.textContent = data[key];
+      if (messages[lang][key]) {
+        el.textContent = messages[lang][key];
       }
     });
-  });
+}
---
+++ js/message.js
@@ -0,0 +1,15 @@
+export const messages = {
+  ja: {
+    about_me: "メンテナンス中",
+    skills: "メンテナンス中",
+    works: "メンテナンス中",
+    blogs: "メンテナンス中",
+    contact: "メンテナンス中"  },
+  en: {
+    about_me: "Comming soon!",
+    skills: "Comming soon!",
+    works: "Comming soon!",
+    blogs: "Comming soon!",
+    contact: "Comming soon!"
+  }
+};
